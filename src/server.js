9289d3299681fd8966fa835f7bdcbccade25bd7c
Diego Ruguê <diegorugue@gmail.com>
2020-09-04 20:17:39 -0300

    start to v2 refactor

--- src/server.js
+++
@@ -1,5 +0,0 @@
-const app = require('./app');
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('API rodando na porta 3000');
-});
--- src/utils/db/truncate.js
+++
@@ -1,9 +0,0 @@
-const database = require('../../database');
-
-module.exports = function truncate() {
-  return Promise.all(
-    Object.keys(database.connection.models).map(key => {
-      return database.connection.models[key].destroy({ truncate: true, force: true });
-    }),
-  );
-};
